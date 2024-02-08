bcd8d8a0aa22b2572395b018347d5ac69a98a1c7
Maciej Ziaja <maciejzjg@gmail.com>
2024-02-08 10:08:08 +0100

    Bump ssb version to v0.1.0

--- ssb.rb
+++ ssb.rb
@@ -2,7 +2,7 @@ class Ssb < Formula
   desc "Static site generator and blogging anti-framework"
   homepage "https://github.com/maciejzj/ssb"
   url "https://github.com/Maciejzj/ssb.git", revision: "808c149bc699e47cb51a2fb64e023a0d2c13f2eb"
-  version "0.0.2"
+  version "0.1.0"
   license "MIT"
 
   option "without-pandoc", "Install without default pandoc markdown renderer"
